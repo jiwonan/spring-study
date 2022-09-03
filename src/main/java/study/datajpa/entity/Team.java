9eb44c5758c625df9b25b28759be80e8f4ffdd41
jiwonAn1 <jwan0131@gmail.com>
2022-09-03 23:50:32 +0900

    테스트를 위한 엔티티 세팅

---
+++ src/main/java/study/datajpa/entity/Team.java
@@ -0,0 +1,31 @@
+package study.datajpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private int id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(int id, String name, List<Member> members) {
+        this.id = id;
+        this.name = name;
+        this.members = members;
+    }
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
