ecdf5d1a1db28b193475e5db01bf0b2ec048277a
jiwonAn1 <jwan0131@gmail.com>
2022-09-04 15:13:19 +0900

    Auditing

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -280,4 +280,26 @@ class MemberRepositoryTest {
     public void callCustom() {
         List<Member> result = memberRepository.findMemberCustom();
     }
+
+    @Test
+    public void JpaEventBaseEntity() throws Exception {
+        // given
+        Member member = new Member("member1");
+        memberRepository.save(member);
+
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        em.flush();
+        em.clear();
+
+        // when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        // then
+        System.out.println("findMember.createdDate : " + findMember.getCreatedDate());
+        System.out.println("findMember.updatedDate : " + findMember.getLastModifiedDate());
+        System.out.println("findMember.createdBy : " + findMember.getCreatedBy());
+        System.out.println("findMember.lastModifiedBy : " + findMember.getLastModifiedBy());
+    }
 }
